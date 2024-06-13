ed390504df78a15477be1bc43598d319e4423111
Martin Evans <martindevans@gmail.com>
2024-06-14 00:25:35 +0100

    Fixed ParallelTasks build

---
+++ ParallelTasks/Polyfill/IsExternalInit.cs
@@ -0,0 +1,16 @@
+﻿// ReSharper disable All
+namespace System.Runtime.CompilerServices;
+
+#if !NET6_0_OR_GREATER
+
+using Diagnostics;
+using Diagnostics.CodeAnalysis;
+
+/// <summary>
+/// Reserved to be used by the compiler for tracking metadata.
+/// This class should not be used by developers in source code.
+/// </summary>
+[ExcludeFromCodeCoverage, DebuggerNonUserCode]
+internal static class IsExternalInit;
+
+#endif
