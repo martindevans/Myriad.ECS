c7ad1405dae1d62a4d9fe6e87c21d4f86ab3c5be
Martin Evans <martindevans@gmail.com>
2024-06-19 20:16:23 +0100

    Included Myriad.ECS.Networking project

---
+++ Myriad.ECS.Networking/Extensions/EntityExtensions.cs
@@ -0,0 +1,12 @@
+﻿using HandySerialization;
+
+namespace Myriad.ECS.Networking.Extensions;
+
+public static class EntityExtensions
+{
+    public static void Write<TWriter>(this ref TWriter writer, Entity entity)
+        where TWriter : struct, IByteWriter
+    {
+        throw new NotImplementedException("not generally safe to send Entity over network - IDs are not synced!");
+    }
+}
---
+++ Myriad.ECS.Networking/NetworkEntity.cs
@@ -0,0 +1,21 @@
+﻿using HandySerialization;
+using HandySerialization.Extensions;
+
+namespace Myriad.ECS.Networking
+{
+    //public readonly record struct NetworkEntity(ulong Id)
+    //    : IByteSerializable<NetworkEntity>
+    //{
+    //    public void Write<TWriter>(ref TWriter writer)
+    //        where TWriter : struct, IByteWriter
+    //    {
+    //        writer.Write(Id);
+    //    }
+
+    //    public NetworkEntity Read<TReader>(ref TReader reader)
+    //        where TReader : struct, IByteReader
+    //    {
+    //        return new NetworkEntity(reader.ReadUInt64());
+    //    }
+    //}
+}
