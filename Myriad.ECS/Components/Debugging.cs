1d1b63cc6872ddd74aba8d45f264f7b2cfc44b53
Martin Evans <martindevans@gmail.com>
2024-10-01 14:57:51 +0100

    Added `DebugDisplayName`

---
+++ Myriad.ECS/Components/Debugging.cs
@@ -0,0 +1,8 @@
+﻿namespace Myriad.ECS.Components;
+
+/// <summary>
+/// Friendly name to use when displaying this entity for debugging purposes.
+/// Recommended (but not required) to make this unique!
+/// </summary>
+/// <param name="Name"></param>
+public record struct DebugDisplayName(string Name) : IComponent;
