2c28d1baff217777a00e26d89874c1a1fc97e81a
Martin Evans <martindevans@gmail.com>
2025-03-22 20:14:13 +0000

    Hierarchical transforms

---
+++ Myriad.ECS.Tests/Components/TransformTests.cs
@@ -0,0 +1,169 @@
+﻿using System.Numerics;
+using Myriad.ECS.Command;
+using Myriad.ECS.Components;
+using Myriad.ECS.Systems;
+using Myriad.ECS.Worlds;
+
+namespace Myriad.ECS.Tests.Components;
+
+[TestClass]
+public class TransformTests
+{
+    [TestMethod]
+    public void SingleEntityTransform()
+    {
+        var w = new WorldBuilder().Build();
+        var c = new CommandBuffer(w);
+        var eb = c.Create()
+            .Set(new Vector2LocalTransform { Transform = new(Vector2.One) })
+            .Set(new Vector2WorldTransform());
+
+        using var _ = c.Playback();
+        var e = eb.Resolve();
+
+        new TransformAddIntegers(w).Update(new GameTime());
+
+        Assert.AreEqual(Vector2.One, e.GetComponentRef<Vector2LocalTransform>().Transform.Value);
+        Assert.AreEqual(Vector2.One, e.GetComponentRef<Vector2WorldTransform>().Transform.Value);
+    }
+
+    [TestMethod]
+    public void SingleParentEntityTransform()
+    {
+        var w = new WorldBuilder().Build();
+        var c = new CommandBuffer(w);
+
+        var eb1 = c.Create()
+                  .Set(new Vector2LocalTransform { Transform = new(Vector2.UnitX) })
+                  .Set(new Vector2WorldTransform());
+
+        var eb2 = c.Create()
+                   .Set(new Vector2LocalTransform { Transform = new(Vector2.UnitY) })
+                   .Set(new Vector2WorldTransform())
+                   .Set(new TransformParent(), eb1);
+
+        using var _ = c.Playback();
+        var parent = eb1.Resolve();
+        var child = eb2.Resolve();
+
+        new TransformAddIntegers(w).Update(new GameTime());
+
+        Assert.AreEqual(Vector2.UnitX, parent.GetComponentRef<Vector2LocalTransform>().Transform.Value);
+        Assert.AreEqual(Vector2.UnitX, parent.GetComponentRef<Vector2WorldTransform>().Transform.Value);
+
+        Assert.AreEqual(Vector2.UnitY, child.GetComponentRef<Vector2LocalTransform>().Transform.Value);
+        Assert.AreEqual(Vector2.One, child.GetComponentRef<Vector2WorldTransform>().Transform.Value);
+    }
+
+    [TestMethod]
+    public void TwoChildrenParentEntityTransform()
+    {
+        var w = new WorldBuilder().Build();
+        var c = new CommandBuffer(w);
+
+        var eb1 = c.Create()
+                   .Set(new Vector2LocalTransform { Transform = new(Vector2.UnitX) })
+                   .Set(new Vector2WorldTransform());
+
+        var eb2 = c.Create()
+                   .Set(new Vector2LocalTransform { Transform = new(Vector2.UnitY) })
+                   .Set(new Vector2WorldTransform())
+                   .Set(new TransformParent(), eb1);
+
+        var eb3 = c.Create()
+                   .Set(new Vector2LocalTransform { Transform = new(-Vector2.UnitY) })
+                   .Set(new Vector2WorldTransform())
+                   .Set(new TransformParent(), eb1);
+
+        using var _ = c.Playback();
+        var parent = eb1.Resolve();
+        var child2 = eb2.Resolve();
+        var child3 = eb3.Resolve();
+
+        new TransformAddIntegers(w).Update(new GameTime());
+
+        Assert.AreEqual(Vector2.UnitX, parent.GetComponentRef<Vector2LocalTransform>().Transform.Value);
+        Assert.AreEqual(Vector2.UnitX, parent.GetComponentRef<Vector2WorldTransform>().Transform.Value);
+
+        Assert.AreEqual(Vector2.UnitY, child2.GetComponentRef<Vector2LocalTransform>().Transform.Value);
+        Assert.AreEqual(Vector2.One, child2.GetComponentRef<Vector2WorldTransform>().Transform.Value);
+
+        Assert.AreEqual(-Vector2.UnitY, child3.GetComponentRef<Vector2LocalTransform>().Transform.Value);
+        Assert.AreEqual(new Vector2(1, -1), child3.GetComponentRef<Vector2WorldTransform>().Transform.Value);
+    }
+
+    [TestMethod]
+    public void MultipleChainedChildren()
+    {
+        var w = new WorldBuilder().Build();
+        var c = new CommandBuffer(w);
+
+        var eb1 = c.Create()
+                   .Set(new Vector2LocalTransform { Transform = new(Vector2.UnitX) })
+                   .Set(new Vector2WorldTransform());
+
+        var eb2 = c.Create()
+                   .Set(new Vector2LocalTransform { Transform = new(Vector2.UnitY) })
+                   .Set(new Vector2WorldTransform())
+                   .Set(new TransformParent(), eb1);
+
+        var eb3 = c.Create()
+                   .Set(new Vector2LocalTransform { Transform = new(2 * Vector2.UnitY) })
+                   .Set(new Vector2WorldTransform())
+                   .Set(new TransformParent(), eb2);
+
+        using var _ = c.Playback();
+        var parent = eb1.Resolve();
+        var child2 = eb2.Resolve();
+        var child3 = eb3.Resolve();
+
+        new TransformAddIntegers(w).Update(new GameTime());
+
+        Assert.AreEqual(Vector2.UnitX, parent.GetComponentRef<Vector2LocalTransform>().Transform.Value);
+        Assert.AreEqual(Vector2.UnitX, parent.GetComponentRef<Vector2WorldTransform>().Transform.Value);
+
+        Assert.AreEqual(Vector2.UnitY, child2.GetComponentRef<Vector2LocalTransform>().Transform.Value);
+        Assert.AreEqual(Vector2.One, child2.GetComponentRef<Vector2WorldTransform>().Transform.Value);
+
+        Assert.AreEqual(2 * Vector2.UnitY, child3.GetComponentRef<Vector2LocalTransform>().Transform.Value);
+        Assert.AreEqual(new Vector2(1, 3), child3.GetComponentRef<Vector2WorldTransform>().Transform.Value);
+    }
+
+    private class TransformAddIntegers
+        : BaseUpdateTransformHierarchySystem<GameTime, Vector2Transform, Vector2LocalTransform, Vector2WorldTransform>
+    {
+        public TransformAddIntegers(World world)
+            : base(world)
+        {
+        }
+    }
+
+    private struct Vector2Transform
+        : ITransform<Vector2Transform>
+    {
+        public Vector2 Value;
+
+        public Vector2Transform(Vector2 value)
+        {
+            Value = value;
+        }
+
+        public Vector2Transform Compose(Vector2Transform child)
+        {
+            return new Vector2Transform { Value = Value + child.Value };
+        }
+    }
+
+    private struct Vector2LocalTransform
+        : ILocalTransform<Vector2Transform>
+    {
+        public Vector2Transform Transform { get; set; }
+    }
+
+    private struct Vector2WorldTransform
+        : IWorldTransform<Vector2Transform>
+    {
+        public Vector2Transform Transform { get; set; }
+        public int Phase { get; set; }
+    }
+}
---
+++ Myriad.ECS/Components/Transform.cs
@@ -0,0 +1,200 @@
+﻿using Myriad.ECS.Queries;
+using Myriad.ECS.Systems;
+using Myriad.ECS.Worlds;
+
+namespace Myriad.ECS.Components;
+
+/// <summary>
+/// Represents a transform
+/// </summary>
+public interface ITransform<TSelf>
+    where TSelf : ITransform<TSelf>
+{
+    /// <summary>
+    /// Given a child transform, compose it with this transform
+    /// </summary>
+    /// <param name="child"></param>
+    /// <returns></returns>
+    TSelf Compose(TSelf child);
+}
+
+/// <summary>
+/// Indicates which entity this entity is transformed relative to
+/// </summary>
+public struct TransformParent
+    : IEntityRelationComponent
+{
+    /// <summary>
+    /// Parent entity transform
+    /// </summary>
+    public Entity Target { get; set; }
+}
+
+/// <summary>
+/// Transform of this entity, relative to parent
+/// </summary>
+public interface ILocalTransform<TTransform>
+    : IComponent
+    where TTransform : ITransform<TTransform>
+{
+    /// <summary>
+    /// Get or set the local transform
+    /// </summary>
+    TTransform Transform { get; set; }
+}
+
+/// <summary>
+/// Transform of this entity, in world space.
+/// </summary>
+public interface IWorldTransform<TTransform>
+    : IComponent
+    where TTransform : ITransform<TTransform>
+{
+    /// <summary>
+    /// Get or set the local transform
+    /// </summary>
+    TTransform Transform { get; set; }
+
+    /// <summary>
+    /// Indicates to the <see cref="BaseUpdateTransformHierarchySystem{TData,TTransform,TLocalTransform,TWorldTransform}"/> when this world transform was last updated
+    /// </summary>
+    int Phase { get; set; }
+}
+
+/// <summary>
+/// Automatically updates hierarchy of entities, deriving world transform from local transform and parent.
+/// </summary>
+/// <typeparam name="TData"></typeparam>
+/// <typeparam name="TLocalTransform"></typeparam>
+/// <typeparam name="TWorldTransform"></typeparam>
+/// <typeparam name="TTransform"></typeparam>
+public class BaseUpdateTransformHierarchySystem<TData, TTransform, TLocalTransform, TWorldTransform>
+    : ISystem<TData>
+    where TLocalTransform : ILocalTransform<TTransform>
+    where TWorldTransform : IWorldTransform<TTransform>
+    where TTransform : ITransform<TTransform>
+{
+    private readonly World _world;
+    private readonly QueryDescription _queryRoot;
+    private readonly QueryDescription _query;
+
+    private int _phase;
+
+    /// <summary>
+    /// Create new <see cref="BaseUpdateTransformHierarchySystem{TData,TTransform,TLocalTransform,TWorldTransform}"/>
+    /// </summary>
+    /// <param name="world"></param>
+    public BaseUpdateTransformHierarchySystem(World world)
+    {
+        _world = world;
+        _queryRoot = new QueryBuilder().Include<TLocalTransform, TWorldTransform>().Exclude<TransformParent>().Build(world);
+        _query     = new QueryBuilder().Include<TLocalTransform, TWorldTransform>().Include<TransformParent>().Build(world);
+    }
+
+    /// <inheritdoc />
+    public void Update(TData data)
+    {
+        // Move to the next phase
+        unchecked { _phase++; }
+
+        // Update roots, just copying across transform from local to world
+        _world.Execute<RootUpdate, TLocalTransform, TWorldTransform>(new RootUpdate(_phase), _queryRoot);
+
+        // Recursively update children (walking up tree from entities to parent)
+        _world.Execute<RecursiveUpdate, TLocalTransform, TWorldTransform, TransformParent>(new RecursiveUpdate(_phase), _query);
+    }
+
+    private static void Update(int phaseDone, ref TLocalTransform local, ref TWorldTransform world)
+    {
+        if (world.Phase == phaseDone)
+            return;
+
+        world.Transform = local.Transform;
+        world.Phase = phaseDone;
+    }
+
+    private static void Update(int phaseDone, int phaseWip, ref TLocalTransform local, ref TWorldTransform world, ref TransformParent parent)
+    {
+        // Early exit if this has already been updated in this phase
+        if (world.Phase == phaseDone)
+            return;
+
+        // Detect loops and early exit
+        if (world.Phase == phaseWip)
+            return;
+
+        // Mark this object with the WIP flag, to detect and break loops later
+        world.Phase = phaseWip;
+
+        // Check status of parent entity
+        var parentHasWorld = parent.Target.IsAlive() && parent.Target.HasComponent<TWorldTransform>();
+
+        // If the parent doesn't exist or doesn't have a transform, treat this entity as a root
+        if (!parentHasWorld)
+        {
+            Update(phaseDone, ref local, ref world);
+        }
+        else
+        {
+            ref var pWorldTrans = ref parent.Target.GetComponentRef<TWorldTransform>();
+            if (parent.Target.HasComponent<TLocalTransform>())
+            {
+                ref var pLocalTrans = ref parent.Target.GetComponentRef<TLocalTransform>();
+
+                // Recursively update parent
+                if (parent.Target.HasComponent<TransformParent>())
+                    Update(phaseDone, phaseWip, ref pLocalTrans, ref pWorldTrans, ref parent.Target.GetComponentRef<TransformParent>());
+                else
+                    Update(phaseDone, ref pLocalTrans, ref pWorldTrans);
+            }
+            else
+            {
+                // Parent has no local transform. Just mark it as done and transform relative to it.
+                pWorldTrans.Phase = phaseDone;
+            }
+
+            // Transform relative to parent
+            world.Transform = pWorldTrans.Transform.Compose(local.Transform);
+        }
+
+        // Mark this entity as done
+        world.Phase = phaseDone;
+    }
+
+    private readonly struct RootUpdate
+        : IQuery<TLocalTransform, TWorldTransform>
+    {
+        private readonly int _phase;
+
+        public RootUpdate(int phase)
+        {
+            _phase = phase;
+        }
+
+        public void Execute(Entity e, ref TLocalTransform local, ref TWorldTransform world)
+        {
+            Update(_phase, ref local, ref world);
+        }
+    }
+
+    private readonly struct RecursiveUpdate
+        : IQuery<TLocalTransform, TWorldTransform, TransformParent>
+    {
+        private readonly int _phaseDone;
+        private readonly int _phaseWip;
+
+        public RecursiveUpdate(int phase)
+        {
+            _phaseDone = phase;
+
+            // Create another number that's different from the phase number. Doesn't really matter what, as long as it's
+            // always different. This is used to mark entities which are currently being updated, to detect loops.
+            _phaseWip = phase ^ 0b1010101010101010101010101010101;
+        }
+
+        public void Execute(Entity e, ref TLocalTransform local, ref TWorldTransform world, ref TransformParent parent)
+        {
+            Update(_phaseDone, _phaseWip, ref local, ref world, ref parent);
+        }
+    }
+}
