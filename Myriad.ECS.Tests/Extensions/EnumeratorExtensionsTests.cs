cb74553c91e1149f89e5020dc2df03e200827337
Martin Evans <martindevans@gmail.com>
2025-10-20 22:54:39 +0100

    More tests

---
+++ Myriad.ECS.Tests/Extensions/EnumeratorExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using Myriad.ECS.Extensions;
+
+namespace Myriad.ECS.Tests.Extensions;
+
+[TestClass]
+public class EnumeratorExtensionsTests
+{
+    [TestMethod]
+    public void SkipEnumerator()
+    {
+        var e = Enumerable.Range(0, 100).ToList().GetEnumerator();
+        Assert.IsTrue(e.Skip(5));
+
+        Assert.AreEqual(4, e.Current);
+    }
+
+    [TestMethod]
+    public void SkipEnumerator_PastEnd()
+    {
+        var e = Enumerable.Range(0, 10).ToList().GetEnumerator();
+        Assert.IsFalse(e.Skip(15));
+    }
+}
