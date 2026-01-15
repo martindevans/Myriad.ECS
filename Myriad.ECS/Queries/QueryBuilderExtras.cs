e03e3a70bb914ed2aee9ef0a855bee889044c5d3
Martin Evans <martindevans@gmail.com>
2026-01-15 17:47:16 +0000

    Added `IncludeQuery` to query builder

--- Myriad.ECS/Queries/QueryBuilderExtras.cs
+++ Myriad.ECS/Queries/QueryBuilderExtras.cs
@@ -29,6 +29,23 @@ public sealed partial class QueryBuilder
 	/// Include only entities which have all of these components
 	/// </summary>
 	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1>()
+		where TQ : IQuery<T0, T1>
+		where T0 : IComponent
+		where T1 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
 	[ExcludeFromCodeCoverage]
 	public QueryBuilder Include<T0, T1, T2>()
 		where T0 : IComponent
@@ -46,8 +63,48 @@ public sealed partial class QueryBuilder
 	/// Include only entities which have all of these components
 	/// </summary>
 	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1, T2>()
+		where TQ : IQuery<T0, T1, T2>
+		where T0 : IComponent
+		where T1 : IComponent
+		where T2 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+		Include<T2>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
 	[ExcludeFromCodeCoverage]
 	public QueryBuilder Include<T0, T1, T2, T3>()
+		where T0 : IComponent
+		where T1 : IComponent
+		where T2 : IComponent
+		where T3 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+		Include<T2>();
+		Include<T3>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1, T2, T3>()
+		where TQ : IQuery<T0, T1, T2, T3>
 		where T0 : IComponent
 		where T1 : IComponent
 		where T2 : IComponent
@@ -86,8 +143,56 @@ public sealed partial class QueryBuilder
 	/// Include only entities which have all of these components
 	/// </summary>
 	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1, T2, T3, T4>()
+		where TQ : IQuery<T0, T1, T2, T3, T4>
+		where T0 : IComponent
+		where T1 : IComponent
+		where T2 : IComponent
+		where T3 : IComponent
+		where T4 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+		Include<T2>();
+		Include<T3>();
+		Include<T4>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
 	[ExcludeFromCodeCoverage]
 	public QueryBuilder Include<T0, T1, T2, T3, T4, T5>()
+		where T0 : IComponent
+		where T1 : IComponent
+		where T2 : IComponent
+		where T3 : IComponent
+		where T4 : IComponent
+		where T5 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+		Include<T2>();
+		Include<T3>();
+		Include<T4>();
+		Include<T5>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1, T2, T3, T4, T5>()
+		where TQ : IQuery<T0, T1, T2, T3, T4, T5>
 		where T0 : IComponent
 		where T1 : IComponent
 		where T2 : IComponent
@@ -134,8 +239,64 @@ public sealed partial class QueryBuilder
 	/// Include only entities which have all of these components
 	/// </summary>
 	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1, T2, T3, T4, T5, T6>()
+		where TQ : IQuery<T0, T1, T2, T3, T4, T5, T6>
+		where T0 : IComponent
+		where T1 : IComponent
+		where T2 : IComponent
+		where T3 : IComponent
+		where T4 : IComponent
+		where T5 : IComponent
+		where T6 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+		Include<T2>();
+		Include<T3>();
+		Include<T4>();
+		Include<T5>();
+		Include<T6>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
 	[ExcludeFromCodeCoverage]
 	public QueryBuilder Include<T0, T1, T2, T3, T4, T5, T6, T7>()
+		where T0 : IComponent
+		where T1 : IComponent
+		where T2 : IComponent
+		where T3 : IComponent
+		where T4 : IComponent
+		where T5 : IComponent
+		where T6 : IComponent
+		where T7 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+		Include<T2>();
+		Include<T3>();
+		Include<T4>();
+		Include<T5>();
+		Include<T6>();
+		Include<T7>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1, T2, T3, T4, T5, T6, T7>()
+		where TQ : IQuery<T0, T1, T2, T3, T4, T5, T6, T7>
 		where T0 : IComponent
 		where T1 : IComponent
 		where T2 : IComponent
@@ -190,8 +351,72 @@ public sealed partial class QueryBuilder
 	/// Include only entities which have all of these components
 	/// </summary>
 	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1, T2, T3, T4, T5, T6, T7, T8>()
+		where TQ : IQuery<T0, T1, T2, T3, T4, T5, T6, T7, T8>
+		where T0 : IComponent
+		where T1 : IComponent
+		where T2 : IComponent
+		where T3 : IComponent
+		where T4 : IComponent
+		where T5 : IComponent
+		where T6 : IComponent
+		where T7 : IComponent
+		where T8 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+		Include<T2>();
+		Include<T3>();
+		Include<T4>();
+		Include<T5>();
+		Include<T6>();
+		Include<T7>();
+		Include<T8>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
 	[ExcludeFromCodeCoverage]
 	public QueryBuilder Include<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9>()
+		where T0 : IComponent
+		where T1 : IComponent
+		where T2 : IComponent
+		where T3 : IComponent
+		where T4 : IComponent
+		where T5 : IComponent
+		where T6 : IComponent
+		where T7 : IComponent
+		where T8 : IComponent
+		where T9 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+		Include<T2>();
+		Include<T3>();
+		Include<T4>();
+		Include<T5>();
+		Include<T6>();
+		Include<T7>();
+		Include<T8>();
+		Include<T9>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1, T2, T3, T4, T5, T6, T7, T8, T9>()
+		where TQ : IQuery<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9>
 		where T0 : IComponent
 		where T1 : IComponent
 		where T2 : IComponent
@@ -254,8 +479,80 @@ public sealed partial class QueryBuilder
 	/// Include only entities which have all of these components
 	/// </summary>
 	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>()
+		where TQ : IQuery<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>
+		where T0 : IComponent
+		where T1 : IComponent
+		where T2 : IComponent
+		where T3 : IComponent
+		where T4 : IComponent
+		where T5 : IComponent
+		where T6 : IComponent
+		where T7 : IComponent
+		where T8 : IComponent
+		where T9 : IComponent
+		where T10 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+		Include<T2>();
+		Include<T3>();
+		Include<T4>();
+		Include<T5>();
+		Include<T6>();
+		Include<T7>();
+		Include<T8>();
+		Include<T9>();
+		Include<T10>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
 	[ExcludeFromCodeCoverage]
 	public QueryBuilder Include<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>()
+		where T0 : IComponent
+		where T1 : IComponent
+		where T2 : IComponent
+		where T3 : IComponent
+		where T4 : IComponent
+		where T5 : IComponent
+		where T6 : IComponent
+		where T7 : IComponent
+		where T8 : IComponent
+		where T9 : IComponent
+		where T10 : IComponent
+		where T11 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+		Include<T2>();
+		Include<T3>();
+		Include<T4>();
+		Include<T5>();
+		Include<T6>();
+		Include<T7>();
+		Include<T8>();
+		Include<T9>();
+		Include<T10>();
+		Include<T11>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>()
+		where TQ : IQuery<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>
 		where T0 : IComponent
 		where T1 : IComponent
 		where T2 : IComponent
@@ -326,8 +623,88 @@ public sealed partial class QueryBuilder
 	/// Include only entities which have all of these components
 	/// </summary>
 	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>()
+		where TQ : IQuery<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>
+		where T0 : IComponent
+		where T1 : IComponent
+		where T2 : IComponent
+		where T3 : IComponent
+		where T4 : IComponent
+		where T5 : IComponent
+		where T6 : IComponent
+		where T7 : IComponent
+		where T8 : IComponent
+		where T9 : IComponent
+		where T10 : IComponent
+		where T11 : IComponent
+		where T12 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+		Include<T2>();
+		Include<T3>();
+		Include<T4>();
+		Include<T5>();
+		Include<T6>();
+		Include<T7>();
+		Include<T8>();
+		Include<T9>();
+		Include<T10>();
+		Include<T11>();
+		Include<T12>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
 	[ExcludeFromCodeCoverage]
 	public QueryBuilder Include<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>()
+		where T0 : IComponent
+		where T1 : IComponent
+		where T2 : IComponent
+		where T3 : IComponent
+		where T4 : IComponent
+		where T5 : IComponent
+		where T6 : IComponent
+		where T7 : IComponent
+		where T8 : IComponent
+		where T9 : IComponent
+		where T10 : IComponent
+		where T11 : IComponent
+		where T12 : IComponent
+		where T13 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+		Include<T2>();
+		Include<T3>();
+		Include<T4>();
+		Include<T5>();
+		Include<T6>();
+		Include<T7>();
+		Include<T8>();
+		Include<T9>();
+		Include<T10>();
+		Include<T11>();
+		Include<T12>();
+		Include<T13>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>()
+		where TQ : IQuery<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>
 		where T0 : IComponent
 		where T1 : IComponent
 		where T2 : IComponent
@@ -406,8 +783,96 @@ public sealed partial class QueryBuilder
 	/// Include only entities which have all of these components
 	/// </summary>
 	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>()
+		where TQ : IQuery<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>
+		where T0 : IComponent
+		where T1 : IComponent
+		where T2 : IComponent
+		where T3 : IComponent
+		where T4 : IComponent
+		where T5 : IComponent
+		where T6 : IComponent
+		where T7 : IComponent
+		where T8 : IComponent
+		where T9 : IComponent
+		where T10 : IComponent
+		where T11 : IComponent
+		where T12 : IComponent
+		where T13 : IComponent
+		where T14 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+		Include<T2>();
+		Include<T3>();
+		Include<T4>();
+		Include<T5>();
+		Include<T6>();
+		Include<T7>();
+		Include<T8>();
+		Include<T9>();
+		Include<T10>();
+		Include<T11>();
+		Include<T12>();
+		Include<T13>();
+		Include<T14>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
 	[ExcludeFromCodeCoverage]
 	public QueryBuilder Include<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>()
+		where T0 : IComponent
+		where T1 : IComponent
+		where T2 : IComponent
+		where T3 : IComponent
+		where T4 : IComponent
+		where T5 : IComponent
+		where T6 : IComponent
+		where T7 : IComponent
+		where T8 : IComponent
+		where T9 : IComponent
+		where T10 : IComponent
+		where T11 : IComponent
+		where T12 : IComponent
+		where T13 : IComponent
+		where T14 : IComponent
+		where T15 : IComponent
+	{
+		Include<T0>();
+		Include<T1>();
+		Include<T2>();
+		Include<T3>();
+		Include<T4>();
+		Include<T5>();
+		Include<T6>();
+		Include<T7>();
+		Include<T8>();
+		Include<T9>();
+		Include<T10>();
+		Include<T11>();
+		Include<T12>();
+		Include<T13>();
+		Include<T14>();
+		Include<T15>();
+
+		return this;
+	}
+
+	/// <summary>
+	/// Include only entities which have all of these components
+	/// </summary>
+	/// <returns>The query builder</returns>
+	// ReSharper disable once UnusedTypeParameter (Justification: Used for checking the query against the type signature)
+	[ExcludeFromCodeCoverage]
+	public QueryBuilder IncludeQuery<TQ, T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>()
+		where TQ : IQuery<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>
 		where T0 : IComponent
 		where T1 : IComponent
 		where T2 : IComponent
