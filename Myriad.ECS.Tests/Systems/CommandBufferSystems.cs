adf42d703d4ef6b7a31cc4582c566c678e3ad957
Martin Evans <martindevans@gmail.com>
2026-03-05 18:54:13 +0000

    Code coverage for command buffer system subscribers

--- Myriad.ECS.Tests/Systems/CommandBufferSystems.cs
+++ Myriad.ECS.Tests/Systems/CommandBufferSystems.cs
@@ -1,4 +1,5 @@
-﻿using Myriad.ECS.Systems;
+﻿using Myriad.ECS.Command;
+using Myriad.ECS.Systems;
 using Myriad.ECS.Worlds;
 
 namespace Myriad.ECS.Tests.Systems;
@@ -29,13 +30,23 @@ public class CommandBufferSystems
         var world = new WorldBuilder().Build();
         var sys = new CommandBufferSystem<object>(world);
 
+        var sub = new TestSubscriber();
+        sys.Subscribe(sub);
+
         var list = new List<Entity>();
         var buffered = sys.Buffer.Create();
         buffered.DelayedResolve(list);
+        sub.Expect(buffered);
 
         Assert.AreEqual(0, list.Count);
         sys.Update(new());
         Assert.AreEqual(1, list.Count);
+
+        sys.Unsubscribe(sub);
+        sub.AllowCallback(false);
+
+        sys.Buffer.Create();
+        sys.Update(new());
     }
 
     [TestMethod]
@@ -53,5 +64,34 @@ public class CommandBufferSystems
         Assert.AreEqual(0, list.Count);
         sys.AfterUpdate(new());
         Assert.AreEqual(1, list.Count);
+    }
+
+    private class TestSubscriber
+        : ICommandBufferSystemSubscriber
+    {
+        private readonly List<CommandBuffer.BufferedEntity> _expected = [ ];
+        private bool _allowed = true;
+
+        public void Expect(CommandBuffer.BufferedEntity buffered)
+        {
+            _expected.Add(buffered);
+        }
+
+        public void AllowCallback(bool allow = true)
+        {
+            _allowed = allow;
+        }
+
+        public void OnCommandbufferPlayback(CommandBuffer.Resolver resolver)
+        {
+            if (!_allowed)
+                Assert.Fail("Callback not allowed");
+
+            foreach (var bufferedEntity in _expected)
+            {
+                var entity = bufferedEntity.Resolve();
+                Assert.IsTrue(entity.IsAlive());
+            }
+        }
     }
 }
