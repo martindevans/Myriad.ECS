82a6383abeb1410175b5a5a5355e311a34ab31dc
Martin Evans <martindevans@gmail.com>
2026-04-25 20:38:58 +0100

    Removed `ParallelTasks`

--- ParallelTasks/WorkOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ParallelTasks;
-
-/// <summary>
-/// A struct containing options about how an IWork instance can be executed.
-/// </summary>
-/// <param name="MaximumThreads">The maximum number of threads which can concurrently execute this work.</param>
-public readonly record struct WorkOptions(int MaximumThreads = 1);
